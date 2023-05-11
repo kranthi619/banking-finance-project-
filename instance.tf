f43035d818a8902776cbc39b57e7d2a944ce3be0
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-11 09:05:48 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "production-server" {
- ami = "ami-007855ac798b5175e"
- instance_type = "t2.micro"
- availability_zone = "us-east-1a"
- VPC_security_groups = [aws_security_group.ec2group123.id]
- key_name = "jenkinskey1"
- tags = {
- name = "ansible_instance"
- }
- provisioner "remote-exec" {
- inline = [
-     "ansible-playbook bankdeployplaybook.yml"
-     ]
-     }
-     }
+  ami = "ami-0123456789abcdef0" # replace with desired AMI ID
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1a" # specify a zone in ap-south-1 region
+  vpc_security_group_ids = [aws_security_group.ec2group123.id]
+  key_name = "pu-ub.ppk"
+  tags = {
+    name = "ansible_instance"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-playbook bankdeployplaybook.yml"
+    ]
+  }
+}
