a874d3b1061151e2e6baa899b5b0b48456537a57
root <root@ip-172-31-36-117.ap-south-1.compute.internal>
2023-05-12 11:53:15 +0000

    ansj

--- instance.tf
+++ instance.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "insur-proj" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("${path.module}/star-agile-banking-finance-project-babupu-up.ppk")
+    private_key = file("${path.module}/test-server/pu-ub.ppk")
     host        = self.public_ip
   }
   provisioner "remote-exec" {
