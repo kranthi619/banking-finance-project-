c7222ddd3274cb79cec4e6e320544558905d2485
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-12 12:01:14 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "production-server" {
-  ami = "ami-0123456789abcdef0" # replace with desired AMI ID
+  ami = " ami-02eb7a4783e7e9317" # replace with desired AMI ID
   instance_type = "t2.medium"
   availability_zone = "ap-south-1a" # specify a zone in ap-south-1 region
   vpc_security_group_ids = [aws_security_group.ec2group123.id]
