97c04145f5318704c67d0cc60e629ab4976360f8
root <root@ip-172-31-36-117.ap-south-1.compute.internal>
2023-05-12 18:14:22 +0000

    champs

--- instance.tf
+++ instance.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "insur-proj" {
   }
   provisioner "remote-exec" {
     inline = [
-       "chmod 400 /home/ubuntu/star-agile-banking-finance-project-babu/test-server/bank-pro.pem",
+      "chmod 400 /home/ubuntu/star-agile-banking-finance-project-babu/test-server/bank-pro.pem",
       "ansible-playbook /home/ubuntu/star-agile-banking-finance-project-babu/test-server/finance-playbook.yml"
     ]
   }
