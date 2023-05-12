920fc805d63ac7ef164ce2c88fb1eeb5f4439b22
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-12 12:25:10 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,9 +1,20 @@
+resource "aws_security_group" "ec2group123" {
+  name_prefix = "ec2group123-"
+  vpc_id      = "vpc-06c5a53d1e3aa7669"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "production-server" {
-  ami = "ami-02eb7a4783e7e9317" # replace with desired AMI ID
+  ami = "ami-02eb7a4783e7e9317"
   instance_type = "t2.medium"
-  availability_zone = "ap-south-1a" # specify a zone in ap-south-1 region
-  vpc_security_group_ids = [aws_security_group.sg-0888c23f07272012c.id]
-  subnet_id = "<YOUR SUBNET ID>"
+  availability_zone = "ap-south-1a"
+  vpc_security_group_ids = [aws_security_group.ec2group123.id]
   key_name = "pu-ub.ppk"
   tags = {
     name = "ansible_instance"
