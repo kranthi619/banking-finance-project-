37e5ee30f6e5209ee386535a47f54101fb0c2c0f
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-13 21:09:20 +0530

    Update provider.tf

--- test-server/provider.tf
+++ test-server/provider.tf
@@ -1,15 +1,5 @@
-resource "aws_instance" "production-server" {
- ami = ""
- instance_type = "t2.micro"
- availability_zone = "ap-south-1a"
- VPC_security_groups = ["ami-02eb7a4783e7e9317"]
- key_name = "sample"
- tags = {
- name = "ansible_instance"
- }
- provisioner "remote-exec" {
- inline = [
-     "ansible-playbook bankdeployplaybook.yml"
-     ]
-     }
-     }
+provider "aws" {
+region     = "ap-south-1" 
+access_key= "AKIA3TGRI6WEPQWGGO7X"
+secret_key= "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"
+}
