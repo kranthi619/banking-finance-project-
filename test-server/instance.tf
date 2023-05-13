d7c2f0dade0bf8a968b55dc037a6bbc4367cd087
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-13 21:56:48 +0530

    Update instance.tf

--- test-server/instance.tf
+++ test-server/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "insur-proj" {
+resource "aws_instance" "ansible_instance" {
   ami                    = "ami-02eb7a4783e7e9317"
   instance_type          = "t2.micro"
   availability_zone      = "ap-south-1a"
