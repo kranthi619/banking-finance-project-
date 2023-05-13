c07cad83543999593b24145b6ee8e6ec4db8dcd6
root <root@ip-172-31-36-117.ap-south-1.compute.internal>
2023-05-13 04:34:02 +0000

    champ

--- instance.tf
+++ instance.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "insur-proj" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("/test-server/bank-pro.pem")
+    private_key = file("/test-server/pu-ub.ppk")
     host        = self.public_ip
   }
   provisioner "remote-exec" {
