e1032c0cff7fd1f922d0b18b3d21b8ce94941280
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-13 20:56:57 +0530

    Update provider.tf

--- test-server/provider.tf
+++ test-server/provider.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "production-server" {
  instance_type = "t2.micro"
  availability_zone = "ap-south-1a"
  VPC_security_groups = ["ami-02eb7a4783e7e9317"]
- key_name = "pu-ub"
+ key_name = "sample"
  tags = {
  name = "ansible_instance"
  }
