cd4205f8329c9e8851ddc9516038ef8c3d59b565
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-14 13:14:54 +0530

    Update variables.tf

--- test-server/variables.tf
+++ test-server/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "terra"
+  default     =  "sample"
 }
 
 variable "instance_type" {
