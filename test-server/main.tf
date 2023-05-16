24dd4a9b1b903c19c3d046382ebfa3d211366800
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-16 19:59:41 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,26 +1,34 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-02eb7a4783e7e9317" 
-  instance_type = "t2.micro" 
-  key_name = "pu-ub"
-  vpc_security_group_ids= ["sg-0888c23f07272012c"]
+  ami           = "ami-02eb7a4783e7e9317"
+  instance_type = "t2.micro"
+  key_name      = "pu-ub"
+  vpc_security_group_ids = ["sg-0888c23f07272012c"]
+  
   connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("/var/lib/jenkins/workspace/bank-pro/test-server/pu-ub.ppk")
-    host     = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "/var/lib/jenkins/workspace/bank-pro/test-server/pu-ub.ppk"
+    host        = self.public_ip
   }
+
   provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
+    inline = [
+      "echo 'wait to start instance'",
+    ]
   }
+
   tags = {
     Name = "test-server"
   }
+
   provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
   }
-   provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/finance-playbook.yml "
-  } 
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/finance-playbook.yml"
+  }
 }
+
 
 
