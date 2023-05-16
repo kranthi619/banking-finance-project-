b9f7c6ee81a0d5571c1efc88c4d1c9a6b50ea18d
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-16 23:54:53 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,13 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "test-server" {
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = "t2.micro"
   key_name      = "exampl"
   vpc_security_group_ids = ["sg-0888c23f07272012c"]
-  
+
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = "/var/lib/jenkins/workspace/bank-pro/test-server/exampl.pem"
+    private_key = file("/root/star-agile-banking-finance-project-babu/test-server/exampl.pem")
     host        = self.public_ip
   }
 
@@ -26,9 +30,10 @@ resource "aws_instance" "test-server" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/finance-playbook.yml"
+    command = "ansible-playbook /root/star-agile-banking-finance-project-babu/test-server/finance-playbook.yml"
   }
 }
+
 
 
 
