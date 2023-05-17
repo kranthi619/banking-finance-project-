79cee48784713d72db65780a369e5bbdf3b26edf
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-17 14:39:48 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -38,13 +38,14 @@ resource "aws_instance" "test-server" {
   }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+    command = "echo '${aws_instance.test-server.public_ip}' > inventory"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/bankdeployplaybook.yml "
+    command = "ansible-playbook -i inventory /var/lib/jenkins/workspace/bank-pro/test-server/bankdeployplaybook.yml "
   }
 }
+
 
 
 
