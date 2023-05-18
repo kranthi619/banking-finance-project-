c62582ea0f4afae91cfc132bf994f031e604b063
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-18 14:30:08 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,36 +1,29 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-02eb7a4783e7e9317" 
-  instance_type = "t2.micro" 
+  ami           = "ami-02eb7a4783e7e9317"
+  instance_type = "t2.micro"
   key_name      = "pu-ub"
-  vpc_security_group_ids = ["sg-094bae658bc36eadb"]
-  
+  vpc_security_group_ids = ["sg-037d13c0e728afaeb"]
+
   connection {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("./pu-ub.ppk")
     host        = self.public_ip
   }
-  
+
   provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
+    inline = ["echo 'wait to start instance' "]
   }
-  
+
   tags = {
     Name = "test-server"
   }
-  
+
   provisioner "local-exec" {
     command = "echo ${aws_instance.test-server.public_ip} > inventory"
   }
-  
+
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/finance-playbook.yml"
-  } 
+    command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/bankdeployplaybook.yml"
+  }
 }
-
-
-
-
-
-
-
