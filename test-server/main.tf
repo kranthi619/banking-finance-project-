7faab1f2e213b7d7c14f2d86da9464ad7f8d4814
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 12:30:26 +0530

    Create main.tf

---
+++ test-server/main.tf
@@ -0,0 +1,31 @@
+
+resource "aws_instance" "test-server" {
+  ami                = "ami-02eb7a4783e7e9317"
+  instance_type      = "t2.micro"
+  key_name           = "exampl"
+  vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("exampl.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance'"]
+  }
+
+  tags = {
+    name = "test-server"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/project2/test-server/bankdeployplaybook.yml"
+  }
+}
+
