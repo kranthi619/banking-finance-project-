8a30178dba1bc865308b4db8403451f10684a57b
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 10:16:30 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,26 +1,12 @@
-resource "aws_security_group" "ec2group123" {
-  name        = "ec2group123"
-  description = "EC2 Security Group"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Add any additional ingress or egress rules as needed
-}
-
-resource "aws_instance" "production-server" {
+resource "aws_instance" "test-server" {
   ami                = "ami-02eb7a4783e7e9317"
   instance_type      = "t2.micro"
   availability_zone  = "ap-south-1b"
-  vpc_security_group_ids = [aws_security_group.ec2group123.id]
+  vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]  # Replace with your existing security group ID
   key_name           = "pu-ub"
 
   tags = {
-    name = "ansible_instance"
+    name = "test-server"
   }
 
   provisioner "remote-exec" {
@@ -29,3 +15,5 @@ resource "aws_instance" "production-server" {
     ]
   }
 }
+
+
