b84de2eed738cb2422e0a9b794348f3cd06970e1
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 09:55:01 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,8 +1,22 @@
+resource "aws_security_group" "ec2group123" {
+  name        = "ec2group123"
+  description = "EC2 Security Group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Add any additional ingress or egress rules as needed
+}
+
 resource "aws_instance" "production-server" {
   ami                = "ami-02eb7a4783e7e9317"
   instance_type      = "t2.micro"
   availability_zone  = "ap-south-1b"
-  vpc_security_group_ids = [aws_security_group.sg-0f10d4bcd49b6d516]
+  vpc_security_group_ids = [aws_security_group.ec2group123.id]
   key_name           = "pu-ub"
 
   tags = {
@@ -15,4 +29,3 @@ resource "aws_instance" "production-server" {
     ]
   }
 }
-
