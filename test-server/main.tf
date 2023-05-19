2fb85e06f74d18dda0f55f549b4c276c61391440
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 11:41:18 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,35 +1,32 @@
+
 resource "aws_instance" "test-server" {
   ami                = "ami-02eb7a4783e7e9317"
   instance_type      = "t2.micro"
-  availability_zone  = "ap-south-1b"
-  vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]  # Replace with your existing security group ID
+  key_name           = "exampl"
+  vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]  
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("exampl")
+    private_key = file("exampl.pem")
     host        = self.public_ip
   }
+
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance'"]
+  }
+
+  tags = {
+    name = "test-server"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/project2/test-server/bankdeployplaybook.yml"
+  }
 }
-
- # provisioner "remote-exec" {
-   # inline = [
-     # "echo 'wait to start instance'"
-   # ]
- # }
-
- # tags = {
-  #  name = "test-server"
- # }
-
-  # provisioner "local-exec" {
-  #  command = "echo ${aws_instance.test-server.public_ip} > inventory"
- # }
-
- # provisioner "local-exec" {
-  #  command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/bankdeployplaybook.yml"
- # }
-# }
-
 
 
