6a18ffbee6cf54cc0efba1dee2b9fee60e9cd2e1
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 10:25:59 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -3,24 +3,32 @@ resource "aws_instance" "test-server" {
   instance_type      = "t2.micro"
   availability_zone  = "ap-south-1b"
   vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]  # Replace with your existing security group ID
-   connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("pu-up.ppk")
-    host     = self.public_ip
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./exampl.pem")
+    host        = self.public_ip
   }
+
   provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
+    inline = [
+      "echo 'wait to start instance'"
+    ]
   }
+
   tags = {
     name = "test-server"
   }
-   provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
   }
-   provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/finance-playbook.yml "
-  } 
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/bankdeployplaybook.yml"
+  }
 }
+
 
 
