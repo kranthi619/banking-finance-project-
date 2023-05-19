6a094239508139c2665370c1f8b3982ee8d3be4a
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 16:02:18 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "test-server" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/project2/test-server/bankdeployplaybook.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/pro-bank2/test-server/bankdeployplaybook.yml"
   }
 }
 
