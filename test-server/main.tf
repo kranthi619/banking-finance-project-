19f0d90c1debb6228dc1ffd34ceb7fc90a36e985
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 09:26:53 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,17 +1,36 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "test-server-sg" {
+  name_prefix = "test-server-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "test-server" {
   ami           = "ami-02eb7a4783e7e9317"
   instance_type = "t2.micro"
   key_name      = "pu-ub"
+  vpc_security_group_ids = [aws_security_group.test-server-sg.id]
+
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("./pu-ub.ppk")
+    private_key = file("exampl.pem")
     host        = self.public_ip
   }
 
   provisioner "remote-exec" {
-    inline = ["echo 'wait to start instance' "]
+    inline = [
+      "echo 'wait to start instance'",
+    ]
   }
 
   tags = {
@@ -19,10 +38,11 @@ resource "aws_instance" "test-server" {
   }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+    command = "echo '${aws_instance.test-server.public_ip}' > inventory"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/bank-pro/test-server/bankdeployplaybook.yml"
+    command = "ansible-playbook -i inventory /var/lib/jenkins/workspace/bank-pro/test-server/bankdeployplaybook.yml "
   }
 }
+
