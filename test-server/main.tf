a804d22b19943b19f8e1cb323664dd4836283ecc
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2023-05-19 11:00:49 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test-server" {
   ami                = "ami-02eb7a4783e7e9317"
   instance_type      = "t2.micro"
-  availability_zone  = "ap-south-1"
+  availability_zone  = "ap-south-1b"
   vpc_security_group_ids = ["sg-0dcfb1d7312730a94"]  # Replace with your existing security group ID
 
   connection {
